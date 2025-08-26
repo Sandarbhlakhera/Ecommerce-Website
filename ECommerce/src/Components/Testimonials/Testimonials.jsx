5fca933823fe9c1023baf9ee0326a49ac0a81a0e
Sandarbh Lakhera <sandylakhera07@gmail.com>
2025-08-26 12:58:57 +0530

    Ecommerce Commit

---
+++ ECommerce/src/Components/Testimonials/Testimonials.jsx
@@ -0,0 +1,119 @@
+import React from 'react'
+import Slider from 'react-slick'
+
+const TestimonialsData=[
+    {
+        id: 1,
+        name: 'Victor',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis dolor tempore quos laudantium modi. Labore, impedit quod laborum, est quam, dignissimos ut expedita iusto saepe a molestias aliquam alias exercitationem?',
+        img: 'https://picsum.photos/200/300?grayscale',
+    },
+    {
+        id: 2,
+        name: 'Satya',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis dolor tempore quos laudantium modi. Labore, impedit quod laborum, est quam, dignissimos ut expedita iusto saepe a molestias aliquam alias exercitationem?',
+        img: 'https://picsum.photos/200/200?grayscale',
+    },
+    {
+        id: 3,
+        name: 'Virat',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis dolor tempore quos laudantium modi. Labore, impedit quod laborum, est quam, dignissimos ut expedita iusto saepe a molestias aliquam alias exercitationem?',
+        img: 'https://picsum.photos/300/200?grayscale',
+    },
+    {
+        id: 4,
+        name: 'Rohit',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis dolor tempore quos laudantium modi. Labore, impedit quod laborum, est quam, dignissimos ut expedita iusto saepe a molestias aliquam alias exercitationem?',
+        img: 'https://picsum.photos/300/300?grayscale',
+    },
+    {
+        id: 5,
+        name: 'Shikhar',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis dolor tempore quos laudantium modi. Labore, impedit quod laborum, est quam, dignissimos ut expedita iusto saepe a molestias aliquam alias exercitationem?',
+        img: 'https://picsum.photos/200/400?grayscale',
+    },
+]
+
+const Testimonials = () => {
+
+    var settings={
+        dots: true,
+        arrows: false,
+        Infinite: true,
+        speed: 500,
+        slideToscroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        cssEase: 'linear',
+        pauseOnHover: true,
+        pauseOnFocus: true,
+        responsive: [
+            {
+                breakpoint: 10000,
+                settings: {
+                    slideToShow: 3,
+                    slideToscroll: 1,
+                    Infinite: true,
+                },
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slideToShow: 2,
+                    slideToscroll: 1,
+                    initialSlider: 2,
+                },
+            },
+            {
+                breakpoint: 640,
+                settings: {
+                    slideToShow: 1,
+                    slideToscroll: 1,
+                },
+            },
+        ],
+    }
+
+  return (
+    <div className='py-10 mb-10'>
+        <div className='container'>
+            <div className='text-center mb-10 max-w-[600px] mx-auto'>
+                <p data-aos='fade-up' className='text-sm text-primary'> What our customers are saying</p>
+                <h1 data-aos='fade-up' className='text-3xl font-bold'> Testomonials</h1>
+                <p data-aos='fade-up' className='text-xs text-gray-400'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, molestiae pariatur. Culpa eligendi possimus, praesentium exercitationem, itaque dicta incidunt aliquam minus aspernatur distinctio hic nulla maiores consequatur odio dolores architecto.
+                </p>
+            </div>
+            <div data-aos='zoom-in'>
+                <Slider {...settings}>
+                    {TestimonialsData.map((data)=>(
+                      <div className='my-6'>
+                        <div
+                         key={data.id}
+                         className='flex flex-col gap-4 shadow-lg py-8 px-6 mx-4 rounded-xl dark:bg-slate-600 bg-primary/10 relative '>
+                            <div className='mb-4'>
+                                <img src={data.img}
+                                alt=''
+                                className='rounded-full w-20 h-20' />
+                            </div>
+                            <div className='flex flex-col items-center gap-4'>
+                                <div className='space-y-3'>
+                                    <p className='text-xs dark:text-slate-300 text-gray-500'>
+                                        {data.text}
+                                    </p>
+                                    <h1 className='text-xl font-bold dark:text-slate-300 text-black/80 dark:text-light'>
+                                    {data.name}</h1>
+                                </div>
+                            </div>
+                            <p className='text-black/20 text-9xl font-serif absolute top-0 right-0'></p>
+                         </div>
+                      </div>  
+                    ))}
+                </Slider>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Testimonials
---
+++ ECommerce/tailwind.config.js
@@ -0,0 +1,26 @@
+import { DefaultContext } from 'react-icons/lib';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,html}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        primary: "#4263eb",
+        secondary: "#63e6be"
+      },
+      container:{
+        center: true,
+        padding:{
+          DEFAULT: '1rem',
+          sm: "3rem",
+        } 
+      }
+    },
+  },
+  darkMode: 'selector',
+  plugins: [],
+}
